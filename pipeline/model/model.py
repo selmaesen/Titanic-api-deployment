b46fad637a5f4a1a6e7784a27237f4829fa469c6
Selma Esen <58880357+selmaesen@users.noreply.github.com>
2021-01-05 16:51:43 +0100

    Update model.py

--- pipeline/model/model.py
+++ pipeline/model/model.py
@@ -2,12 +2,13 @@ import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 import pickle
 
-train_df= pd.read_csv("preprocessed_train_df.csv")
-X_train = train_df.drop(columns=["Survived"])
-y_train = train_df["Survived"] 
+if __name__ == "__main__":
+    train_df= pd.read_csv("preprocessed_train_df.csv")
+    X_train = train_df.drop(columns=["Survived"])
+    y_train = train_df["Survived"] 
 
-knn = KNeighborsClassifier(n_neighbors = 5)
-knn.fit(X_train, y_train)
+    knn = KNeighborsClassifier(n_neighbors = 5)
+    knn.fit(X_train, y_train)
 
-print (knn.score(X_train, y_train)) 
-pickle.dump(knn, open('pipeline/model/model.pkl', 'wb'))
+    print (knn.score(X_train, y_train)) 
+    pickle.dump(knn, open('pipeline/model/model.pkl', 'wb'))
