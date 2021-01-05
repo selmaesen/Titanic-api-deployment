ebcac80be4aaf0a567cdc1fb2521c1a1115adcac
Selma Esen <58880357+selmaesen@users.noreply.github.com>
2021-01-05 16:48:45 +0100

    Update predict.py

--- pipeline/predict/predict.py
+++ pipeline/predict/predict.py
@@ -1,7 +1,7 @@
 import pickle
 
-model_pkl = pickle.load(open('pipeline/model/model.pkl', 'rb'))
 
 def predict(df):
+    model_pkl = pickle.load(open('pipeline/model/model.pkl', 'rb'))
     print (df.shape)
     return model_pkl.predict(df)
